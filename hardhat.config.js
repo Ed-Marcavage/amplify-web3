29dc2785ac4c0757005775ecb8bf84df2bddf5d8
Ed-Marcavage <edmarcavage256@proton.me>
2022-12-02 18:34:09 -0500

    Update for NFT contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,16 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.17",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
+    },
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
@@ -22,16 +31,6 @@ module.exports = {
             chainId: 5,
             blockConfirmations: 6,
         },
-    },
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.7",
-            },
-            {
-                version: "0.6.6",
-            },
-        ],
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
